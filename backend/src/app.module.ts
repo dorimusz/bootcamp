a2398cce2366d65431b167d28d6a7ebb5f4315ba
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-08 15:23:05 +0200

    database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import 'reflect-metadata'; // needed for typeorm
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      // password: process.env.POSTGRES_PASSWORD,
+      password: 'secretpwd',
+      database: 'bootcamp',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
