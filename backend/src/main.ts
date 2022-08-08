550d3aee9ae4c290d825a6a18fb796dfb8acfa30
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-08 13:27:03 +0200

    add typeorm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'reflect-metadata'; // needed for typeorm
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
