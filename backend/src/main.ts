3e553b5897b12314776e97d941e91b098d0c3f3e
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-12 13:29:44 +0200

    add global prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
