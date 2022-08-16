729a3c4a7eb4c1d90bd096e32ccf801206dc8a2f
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-16 15:50:08 +0200

    import all modules and entities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { GithubModule } from './github/github.module';
 
 import 'reflect-metadata'; // needed for typeorm
 import config from '../ormconfig';
+import { Repository } from './entity/repository.entity';
+import { User } from './entity/user.entity';
+import { Contribution } from './entity/contribution.entity';
+
+import { GithubModule } from './github/github.module';
+import { UserModule } from './user/user.module';
+import { RepositoryModule } from './repository/repository.module';
+import { ContributionModule } from './contribution/contribution.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(config), GithubModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forFeature([Repository, User, Contribution]),
+    GithubModule,
+    UserModule,
+    RepositoryModule,
+    ContributionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
 
-//for feature entity class = repository injectable to service
+//forFeature entity class = repository injectable to service
 //repository query builder
