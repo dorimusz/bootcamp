46ed37e512467883c1ec14095cd645782d160f6c
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-26 11:03:39 +0200

    create user endpoints

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, Param, ParseIntPipe } from '@nestjs/common';
+import { ApiResponseService } from 'src/utils/apiResponse.service';
+import { UserService } from '../user/user.service';
+import { Request, Response } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly apiResponseService: ApiResponseService,
+  ) {}
+
+  @Get()
+  async getAllUsers(@Req() req: Request, @Res() res: Response) {
+    const users = await this.userService.getAllUsers();
+    this.apiResponseService.customApiResponse(
+      res,
+      users,
+      'Users cannot be found.',
+      'Error getting users.',
+    );
+  }
+
+  @Get('/:id')
+  async findOneUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ) {
+    const user = await this.userService.getUserById(id);
+    this.apiResponseService.customApiResponse(
+      res,
+      user,
+      'User not found.',
+      'Error getting user.',
+    );
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User as UserEntity } from '../entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return await this.userRepository.find();
+  }
+
+  async getUserById(id: number): Promise<UserEntity> {
+    return await this.userRepository.findOne({
+      where: { userId: id },
+    });
+  }
+}
