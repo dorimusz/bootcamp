7d764ffdfee3d40675b15a7bc9427ca44c49e4ed
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-04 21:44:41 +0200

    add main content container component

---
+++ frontend/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import * as S from "./ContainerAtom";
+import * as T from "../Text/TextAtom";
+import Header from "../Header/Header";
+
+import ReduxTest from "../ReduxTest/ReduxTest";
+
+const Container = () => {
+  return (
+    <S.MainContainer>
+      <Header />
+      {/* <ReduxTest /> */}
+    </S.MainContainer>
+  );
+};
+
+export default Container;
---
+++ frontend/components/Container/ContainerAtom.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Colors } from "../../enums/colorEnums";
+
+export const MainContainer = styled.div`
+  width: 100%;
+  padding-left: 200px;
+  height: 100vh;
+  background: ${Colors.LightBlue};
+`;
