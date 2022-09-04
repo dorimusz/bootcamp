2af8b276e2598fa603b365fca82f6f0f67e679e6
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-04 17:59:01 +0200

    setup redux store

---
+++ frontend/store/index.ts
@@ -0,0 +1,27 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { reducer as userReducer } from "./user/userSlice";
+//Layout children prop
+
+const reducers = {
+  user: userReducer,
+};
+
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware: any) => getDefaultMiddleware(),
+});
+
+//preloaded state
+export const storeWithPreloadedState = (preloadeState: any) => {
+  return configureStore({
+    preloadedState: preloadeState || {},
+    reducer: reducers,
+    middleware: (getDefaultMiddleware: any) => getDefaultMiddleware(),
+  });
+};
+
+export type RootState = ReturnType<typeof store.getState>;
+export type StoreState = ReturnType<typeof store.getState>;
+export type StoreDisptach = typeof store.dispatch;
+
+export default store;
