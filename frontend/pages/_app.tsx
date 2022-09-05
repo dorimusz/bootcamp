e2ea41d299bb2293215f9db5fd5c0d0b76d9c876
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-05 11:17:12 +0200

    remove extra char

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,7 +6,7 @@ import store from "../store";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Provider>
   );
 }
