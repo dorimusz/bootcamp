d9d61f16cad47fb39bdb85f2ef00aaaba6daef8d
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-05 17:21:05 +0200

    add dummy data

---
+++ frontend/content/repo.js
@@ -0,0 +1,11 @@
+const content = {
+  name: "Bloah",
+  owner: "Only god knows",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet hendrerit mauris. Nunc et porta purus. Aliquam tincidunt elit id nisi ultrices ornare. Fusce congue elit vel erat porttitor, sed euismod erat gravida. Suspendisse malesuada orci nec blandit",
+  lang: "JavaScript",
+  star: 419,
+  conts: 420000,
+};
+
+export default content;
