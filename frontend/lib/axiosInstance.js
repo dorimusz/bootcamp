a5e3c80a8dc59b02c403bf51c7bf390554dd3db7
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-06 10:56:15 +0200

    create axios instance

---
+++ frontend/lib/axiosInstance.js
@@ -0,0 +1,32 @@
+import http from "axios";
+
+export const callApi = () => {
+  const instance = http.create({
+    baseURL: "http://localhost:4000/api/v1",
+    timeout: 3000,
+  });
+
+  const get = async (path) => {
+    try {
+      const response = await instance.get(path);
+      return response;
+    } catch (error) {
+      console.log("(get) error status: " + error.response.status);
+      console.log("(get) error data: " + error.response.data);
+      return error.response;
+    }
+  };
+
+  const post = async (path, data) => {
+    try {
+      const response = await instance.post(path, data);
+      // console.log("RESPONSE DATA:", response.data);
+      return response;
+    } catch (error) {
+      console.log("(post) error  data: " + error.response.data);
+      return error.response;
+    }
+  };
+
+  return { get, post, _instance: instance };
+};
