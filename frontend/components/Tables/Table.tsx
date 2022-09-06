fd4c476f6099e50325a9e0faac0fed89f503aac1
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-06 15:06:09 +0200

    create table component (not finished)

---
+++ frontend/components/Tables/Table.tsx
@@ -0,0 +1,7 @@
+import * as S from "./TableAtom";
+
+const Table = () => {
+  return <S.Table></S.Table>;
+};
+
+export default Table;
---
+++ frontend/components/Tables/TableAtom.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Colors } from "../../enums/colorEnums";
+
+export const Table = styled.table``;
+
+export const TableRow = styled.tr``;
+export const TableHeader = styled.th``;
+export const DataCell = styled.td``;
