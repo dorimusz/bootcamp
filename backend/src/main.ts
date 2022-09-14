51238b583e83dea165974a14036db69fb6a16dd2
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-14 11:41:04 +0200

    install and config swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,28 @@
 import 'dotenv/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1'); //kinda can do the same in the app.controller as @Controller() takes a string as a parameter
+
+  const options: SwaggerDocumentOptions = {
+    ignoreGlobalPrefix: true,
+  };
+  const config = new DocumentBuilder()
+    .setTitle('Bootcamp')
+    .setDescription('The Bootcamp API description')
+    .setVersion('1.0')
+    .addTag('Endpoints')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger', app, document);
 
   // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
