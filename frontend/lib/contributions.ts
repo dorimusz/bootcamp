f2dc29c55eecf5a15d98540b5c14f03ad5dd2de1
dorimusz <dorottya.kocsis.it@gmail.com>
2022-09-14 13:20:56 +0200

    get data from different endpoint

--- frontend/lib/contributions.ts
+++ frontend/lib/contributions.ts
@@ -4,7 +4,8 @@ const { get } = apiCall();
 
 export async function getContributionList(id: string) {
   // console.log("@@ID", id);
-  const { data } = await get(`/contribution/repo/${id}`);
+  // const { data } = await get(`/contribution/repo/${id}`);
+  const { data } = await get(`/repository/${id}/contributions`);
   // console.log(data);
   return data;
 }
